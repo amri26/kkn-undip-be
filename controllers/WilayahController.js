40e42e3255126b28ee322edc5d8537fad5e9bf41
GRASSPHIC <tazki.amri@gmail.com>
2023-05-18 08:00:40 +0700

    wilayah modules

--- controllers/WilayahController.js
+++ controllers/WilayahController.js
@@ -5,22 +5,18 @@ const { userSession } = require("../helpers/middleware");
 
 const app = Router();
 
-app.get("/", userSession, async (req, res, next) => {
-    response.sendResponse(res, await modules.listKabupaten());
+app.get("/kabupaten/:id_tema", userSession, async (req, res, next) => {
+    response.sendResponse(
+        res,
+        await modules.listKabupaten(Number(req.params.id_tema))
+    );
 });
 
-app.get(
-    "/kecamatan/:id_tema/:id_kabupaten?",
-    userSession,
-    async (req, res, next) => {
-        response.sendResponse(
-            res,
-            await modules.listKecamatan(
-                Number(req.params.id_tema),
-                req.params.id_kabupaten
-            )
-        );
-    }
-);
+app.get("/kecamatan/:id_kabupaten", userSession, async (req, res, next) => {
+    response.sendResponse(
+        res,
+        await modules.listKecamatan(Number(req.params.id_kabupaten))
+    );
+});
 
 module.exports = app;
--- modules/wilayah.modules.js
+++ modules/wilayah.modules.js
@@ -2,9 +2,28 @@ const { prisma } = require("../helpers/database");
 const Joi = require("joi");
 
 class _wilayah {
-    listKabupaten = async () => {
+    listKabupaten = async (id_tema) => {
         try {
+            const schema = Joi.number().required();
+
+            const validation = schema.validate(id_tema);
+
+            if (validation.error) {
+                const errorDetails = validation.error.details.map(
+                    (detail) => detail.message
+                );
+
+                return {
+                    status: false,
+                    code: 422,
+                    error: errorDetails.join(", "),
+                };
+            }
+
             const list = await prisma.kabupaten.findMany({
+                where: {
+                    id_tema,
+                },
                 include: {
                     _count: {
                         select: {
@@ -28,86 +47,41 @@ class _wilayah {
         }
     };
 
-    listKecamatan = async (id_tema, id_kabupaten) => {
+    listKecamatan = async (id_kabupaten) => {
         try {
-            if (id_kabupaten !== undefined) {
-                const body = {
-                    id_tema,
-                    id_kabupaten: Number(id_kabupaten),
-                };
+            const schema = Joi.number().required();
 
-                const schema = Joi.object({
-                    id_tema: Joi.number().required(),
-                    id_kabupaten: Joi.number().required(),
-                });
+            const validation = schema.validate(id_kabupaten);
 
-                const validation = schema.validate(body);
-
-                if (validation.error) {
-                    const errorDetails = validation.error.details.map(
-                        (detail) => detail.message
-                    );
-
-                    return {
-                        status: false,
-                        code: 422,
-                        error: errorDetails.join(", "),
-                    };
-                }
-
-                const list = await prisma.kecamatan.findMany({
-                    where: {
-                        id_tema: body.id_tema,
-                        id_kabupaten: body.id_kabupaten,
-                    },
-                    include: {
-                        _count: {
-                            select: {
-                                desa: true,
-                            },
-                        },
-                    },
-                });
+            if (validation.error) {
+                const errorDetails = validation.error.details.map(
+                    (detail) => detail.message
+                );
 
                 return {
-                    status: true,
-                    data: list,
-                };
-            } else {
-                const schema = Joi.number().required();
-
-                const validation = schema.validate(id_tema);
-
-                if (validation.error) {
-                    const errorDetails = validation.error.details.map(
-                        (detail) => detail.message
-                    );
-
-                    return {
-                        status: false,
-                        code: 422,
-                        error: errorDetails.join(", "),
-                    };
-                }
-
-                const list = await prisma.kecamatan.findMany({
-                    where: {
-                        id_tema,
-                    },
-                    include: {
-                        _count: {
-                            select: {
-                                desa: true,
-                            },
-                        },
-                    },
-                });
-
-                return {
-                    status: true,
-                    data: list,
+                    status: false,
+                    code: 422,
+                    error: errorDetails.join(", "),
                 };
             }
+
+            const list = await prisma.kecamatan.findMany({
+                where: {
+                    id_kabupaten,
+                },
+                include: {
+                    _count: {
+                        select: {
+                            desa: true,
+                        },
+                    },
+                },
+            });
+
+            return {
+                status: true,
+                data: list,
+            };
         } catch (error) {
             console.error("listKecamatan module error ", error);
 
