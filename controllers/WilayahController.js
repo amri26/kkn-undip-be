5d93c8e796daf70a2b9450a4f0cef8bcc4d01993
GRASSPHIC <tazki.amri@gmail.com>
2023-05-19 11:13:41 +0700

    list wilayah

--- controllers/WilayahController.js
+++ controllers/WilayahController.js
@@ -5,25 +5,21 @@ const { userSession } = require("../helpers/middleware");
 
 const app = Router();
 
-app.get("/kabupaten/:id_tema", userSession, async (req, res, next) => {
+app.get("/:id_tema", userSession, async (req, res, next) => {
     response.sendResponse(
         res,
-        await modules.listKabupaten(Number(req.params.id_tema))
+        await modules.listWilayah(Number(req.params.id_tema))
     );
 });
 
-app.get("/kecamatan", userSession, async (req, res, next) => {
-    if (req.query.id_kabupaten) {
-        response.sendResponse(
-            res,
-            await modules.listKecamatan(Number(req.query.id_kabupaten))
-        );
-    } else if (req.query.id_tema) {
-        response.sendResponse(
-            res,
-            await modules.listKecamatanAll(Number(req.query.id_tema))
-        );
-    }
+app.get("/:id_tema/:id_bappeda", userSession, async (req, res, next) => {
+    response.sendResponse(
+        res,
+        await modules.listMyWilayah(
+            Number(req.params.id_tema),
+            Number(req.params.id_bappeda)
+        )
+    );
 });
 
 module.exports = app;
--- modules/wilayah.modules.js
+++ modules/wilayah.modules.js
@@ -2,7 +2,7 @@ const { prisma } = require("../helpers/database");
 const Joi = require("joi");
 
 class _wilayah {
-    listKabupaten = async (id_tema) => {
+    listWilayah = async (id_tema) => {
         try {
             const schema = Joi.number().required();
 
@@ -25,94 +25,8 @@ class _wilayah {
                     id_tema,
                 },
                 include: {
-                    _count: {
-                        select: {
-                            kecamatan: true,
-                        },
-                    },
-                },
-            });
-
-            return {
-                status: true,
-                data: list,
-            };
-        } catch (error) {
-            console.error("listKabupaten module error ", error);
-
-            return {
-                status: false,
-                error,
-            };
-        }
-    };
-
-    listKecamatanAll = async (id_tema) => {
-        try {
-            const schema = Joi.number().required();
-
-            const validation = schema.validate(id_tema);
-
-            if (validation.error) {
-                const errorDetails = validation.error.details.map(
-                    (detail) => detail.message
-                );
-
-                return {
-                    status: false,
-                    code: 422,
-                    error: errorDetails.join(", "),
-                };
-            }
-
-            const list = await prisma.kabupaten.findMany({
-                where: {
-                    id_tema,
-                },
-                include: {
-                    kecamatan: true,
-                },
-            });
-
-            return {
-                status: true,
-                data: list,
-            };
-        } catch (error) {
-            console.error("listKecamatanAll module error ", error);
-
-            return {
-                status: false,
-                error,
-            };
-        }
-    };
-
-    listKecamatan = async (id_kabupaten) => {
-        try {
-            const schema = Joi.number().required();
-
-            const validation = schema.validate(id_kabupaten);
-
-            if (validation.error) {
-                const errorDetails = validation.error.details.map(
-                    (detail) => detail.message
-                );
-
-                return {
-                    status: false,
-                    code: 422,
-                    error: errorDetails.join(", "),
-                };
-            }
-
-            const list = await prisma.kecamatan.findMany({
-                where: {
-                    id_kabupaten,
-                },
-                include: {
-                    _count: {
-                        select: {
+                    kecamatan: {
+                        include: {
                             desa: true,
                         },
                     },
@@ -124,7 +38,61 @@ class _wilayah {
                 data: list,
             };
         } catch (error) {
-            console.error("listKecamatan module error ", error);
+            console.error("listWilayah module error ", error);
+
+            return {
+                status: false,
+                error,
+            };
+        }
+    };
+
+    listMyWilayah = async (id_tema, id_bappeda) => {
+        try {
+            const body = {
+                id_tema,
+                id_bappeda,
+            };
+
+            const schema = Joi.object({
+                id_tema: Joi.number().required(),
+                id_bappeda: Joi.number().required(),
+            });
+
+            const validation = schema.validate(body);
+
+            if (validation.error) {
+                const errorDetails = validation.error.details.map(
+                    (detail) => detail.message
+                );
+
+                return {
+                    status: false,
+                    code: 422,
+                    error: errorDetails.join(", "),
+                };
+            }
+
+            const list = await prisma.kabupaten.findMany({
+                where: {
+                    id_tema: body.id_tema,
+                    id_bappeda: body.id_bappeda,
+                },
+                include: {
+                    kecamatan: {
+                        include: {
+                            desa: true,
+                        },
+                    },
+                },
+            });
+
+            return {
+                status: true,
+                data: list,
+            };
+        } catch (error) {
+            console.error("listMyWilayah module error ", error);
 
             return {
                 status: false,
