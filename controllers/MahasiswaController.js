f942c6462cbf285eb9bb0a67401b7456b4bdd3ad
oozaw <79121990+oozaw@users.noreply.github.com>
2023-05-30 23:13:05 +0700

    (^) add get list lrk and lpk mahasiswa

--- controllers/MahasiswaController.js
+++ controllers/MahasiswaController.js
@@ -1,73 +1,137 @@
 const { Router } = require("express");
 const modules = require("../modules/mahasiswa.modules");
 const response = require("../helpers/response");
-const { userSession, verifyAdmin, verifyMahasiswa, isActive } = require("../helpers/middleware");
+const {
+  userSession,
+  verifyAdmin,
+  verifyMahasiswa,
+  isActive,
+} = require("../helpers/middleware");
 
 const app = Router();
 
 app.get("/", userSession, verifyAdmin, async (req, res, next) => {
-    response.sendResponse(res, await modules.listMahasiswa());
+  response.sendResponse(res, await modules.listMahasiswa());
 });
 
 app.get("/unregistered", userSession, async (req, res, next) => {
-    response.sendResponse(res, await modules.listMahasiswaUnregistered());
+  response.sendResponse(res, await modules.listMahasiswaUnregistered());
 });
 
 app.get("/registered", userSession, async (req, res, next) => {
-    response.sendResponse(res, await modules.listMahasiswaRegistered());
+  response.sendResponse(res, await modules.listMahasiswaRegistered());
 });
 
 app.get("/accepted", userSession, async (req, res, next) => {
-    response.sendResponse(res, await modules.listMahasiswaAccepted());
+  response.sendResponse(res, await modules.listMahasiswaAccepted());
 });
 
 app.get("/registered/:id_kecamatan", userSession, async (req, res, next) => {
-    response.sendResponse(res, await modules.listMahasiswaRegisteredByKecamatan(Number(req.params.id_kecamatan)));
+  response.sendResponse(
+    res,
+    await modules.listMahasiswaRegisteredByKecamatan(
+      Number(req.params.id_kecamatan)
+    )
+  );
 });
 
 app.get("/accepted/:id_kecamatan", userSession, async (req, res, next) => {
-    response.sendResponse(res, await modules.listMahasiswaAcceptedByKecamatan(Number(req.params.id_kecamatan)));
+  response.sendResponse(
+    res,
+    await modules.listMahasiswaAcceptedByKecamatan(
+      Number(req.params.id_kecamatan)
+    )
+  );
 });
 
-app.post("/daftar_lokasi", userSession, verifyMahasiswa, async (req, res, next) => {
-    const check = await isActive(req.body.id_tema, Number(process.env.MAHASISWA_DAFTAR_LOKASI));
+app.post(
+  "/daftar_lokasi",
+  userSession,
+  verifyMahasiswa,
+  async (req, res, next) => {
+    const check = await isActive(
+      req.body.id_tema,
+      Number(process.env.MAHASISWA_DAFTAR_LOKASI)
+    );
 
     if (!check.status) {
-        response.sendResponse(res, check);
+      response.sendResponse(res, check);
     } else {
-        req.body.id_tema_halaman = check.data.id_tema_halaman;
-        response.sendResponse(res, await modules.daftarLokasi(req.user.id, req.body));
+      req.body.id_tema_halaman = check.data.id_tema_halaman;
+      response.sendResponse(
+        res,
+        await modules.daftarLokasi(req.user.id, req.body)
+      );
     }
+  }
+);
+
+app.get("/lrk", userSession, verifyMahasiswa, async (req, res, next) => {
+  // const check = await isActive(
+  //   req.body.id_tema,
+  //   Number(process.env.MAHASISWA_INDEX_LRK)
+  // );
+
+  // if (!check.status) {
+  //   response.sendResponse(res, check);
+  // } else {
+  response.sendResponse(res, await modules.listLaporan(req.user.id, "lrk"));
+  // }
 });
 
 app.post("/lrk", userSession, verifyMahasiswa, async (req, res, next) => {
-    const check = await isActive(req.body.id_tema, Number(process.env.MAHASISWA_LRK));
+  const check = await isActive(
+    req.body.id_tema,
+    Number(process.env.MAHASISWA_ADD_LRK)
+  );
 
-    if (!check.status) {
-        response.sendResponse(res, check);
-    } else {
-        response.sendResponse(res, await modules.addLRK(req.user.id, req.body));
-    }
+  if (!check.status) {
+    response.sendResponse(res, check);
+  } else {
+    response.sendResponse(res, await modules.addLRK(req.user.id, req.body));
+  }
+});
+
+app.get("/lpk", userSession, verifyMahasiswa, async (req, res, next) => {
+  const check = await isActive(
+    req.body.id_tema,
+    Number(process.env.MAHASISWA_INDEX_LPK)
+  );
+
+  if (!check.status) {
+    response.sendResponse(res, check);
+  } else {
+    response.sendResponse(res, await modules.listLaporan(req.user.id, "lpk"));
+  }
 });
 
 app.post("/lpk", userSession, verifyMahasiswa, async (req, res, next) => {
-    const check = await isActive(req.body.id_tema, Number(process.env.MAHASISWA_LPK));
+  const check = await isActive(
+    req.body.id_tema,
+    Number(process.env.MAHASISWA_ADD_LPK)
+  );
 
-    if (!check.status) {
-        response.sendResponse(res, check);
-    } else {
-        response.sendResponse(res, await modules.addLPK(req.user.id, req.body));
-    }
+  if (!check.status) {
+    response.sendResponse(res, check);
+  } else {
+    response.sendResponse(res, await modules.addLPK(req.user.id, req.body));
+  }
 });
 
 app.post("/reportase", userSession, verifyMahasiswa, async (req, res, next) => {
-    const check = await isActive(req.body.id_tema, Number(process.env.MAHASISWA_REPORTASE));
+  const check = await isActive(
+    req.body.id_tema,
+    Number(process.env.MAHASISWA_REPORTASE)
+  );
 
-    if (!check.status) {
-        response.sendResponse(res, check);
-    } else {
-        response.sendResponse(res, await modules.addReportase(req.user.id, req.body));
-    }
+  if (!check.status) {
+    response.sendResponse(res, check);
+  } else {
+    response.sendResponse(
+      res,
+      await modules.addReportase(req.user.id, req.body)
+    );
+  }
 });
 
 module.exports = app;
--- modules/laporan.modules.js
+++ modules/laporan.modules.js
@@ -2,23 +2,23 @@ const { prisma } = require("../helpers/database");
 const Joi = require("joi");
 
 class _laporan {
-    listLaporan = async () => {
-        try {
-            const list = await prisma.laporan.findMany();
+  listLaporan = async () => {
+    try {
+      const list = await prisma.laporan.findMany();
 
-            return {
-                status: true,
-                data: list,
-            };
-        } catch (error) {
-            console.error("listLaporan module error ", error);
+      return {
+        status: true,
+        data: list,
+      };
+    } catch (error) {
+      console.error("listLaporan module error ", error);
 
-            return {
-                status: false,
-                error,
-            };
-        }
-    };
+      return {
+        status: false,
+        error,
+      };
+    }
+  };
 }
 
 module.exports = new _laporan();
