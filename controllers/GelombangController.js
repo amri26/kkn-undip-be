1640aef3aa8df47ad7149ceef64967efcbb02ac5
oozaw <79121990+oozaw@users.noreply.github.com>
2023-06-01 20:59:19 +0700

    (+) list gelombang dosen

--- controllers/GelombangController.js
+++ controllers/GelombangController.js
@@ -5,6 +5,31 @@ const { userSession } = require("../helpers/middleware");
 
 const app = Router();
 
+app.get("/:id_tema/:id_halaman", userSession, async (req, res, next) => {
+  response.sendResponse(
+    res,
+    await modules.listGelombang(
+      Number(req.params.id_tema),
+      Number(req.params.id_halaman)
+    )
+  );
+});
+
+app.get(
+  "/dosen/:id_tema/:id_halaman/:id_dosen",
+  userSession,
+  async (req, res, next) => {
+    response.sendResponse(
+      res,
+      await modules.listGelombangDosen(
+        Number(req.params.id_tema),
+        Number(req.params.id_halaman),
+        Number(req.params.id_dosen)
+      )
+    );
+  }
+);
+
 app.get(
   "/mahasiswa/:id_tema/:id_halaman/:id_mahasiswa",
   userSession,
@@ -19,15 +44,5 @@ app.get(
     );
   }
 );
-
-app.get("/:id_tema/:id_halaman", userSession, async (req, res, next) => {
-  response.sendResponse(
-    res,
-    await modules.listGelombang(
-      Number(req.params.id_tema),
-      Number(req.params.id_halaman)
-    )
-  );
-});
 
 module.exports = app;
