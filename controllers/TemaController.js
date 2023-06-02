7c5c9a1d57883cb5f6182180c6f3ec00872fd9e2
oozaw <79121990+oozaw@users.noreply.github.com>
2023-06-02 13:17:24 +0700

    (+) list tema dosen

--- controllers/TemaController.js
+++ controllers/TemaController.js
@@ -1,12 +1,16 @@
 const { Router } = require("express");
 const modules = require("../modules/tema.modules");
 const response = require("../helpers/response");
-const { userSession } = require("../helpers/middleware");
+const { userSession, verifyDosen } = require("../helpers/middleware");
 
 const app = Router();
 
 app.get("/", userSession, async (req, res, next) => {
-    response.sendResponse(res, await modules.listTema());
+  response.sendResponse(res, await modules.listTema());
+});
+
+app.get("/dosen", userSession, verifyDosen, async (req, res, next) => {
+  response.sendResponse(res, await modules.listTemaDosen(req.user.id));
 });
 
 module.exports = app;
