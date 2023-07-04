d5a940590145b4f083fea849b393f914e06e475f
oozaw <79121990+oozaw@users.noreply.github.com>
2023-07-04 13:38:41 +0700

    (+) get nilai method on nilai module

--- modules/nilai.modules.js
+++ modules/nilai.modules.js
@@ -2,61 +2,117 @@ const { prisma } = require("../helpers/database");
 const Joi = require("joi");
 
 class _nilai {
-    listNilaiKecamatan = async (id_kecamatan) => {
-        try {
-            const schema = Joi.number().required();
+  listNilaiKecamatan = async (id_kecamatan) => {
+    try {
+      const schema = Joi.number().required();
 
-            const validation = schema.validate(id_kecamatan);
+      const validation = schema.validate(id_kecamatan);
 
-            if (validation.error) {
-                const errorDetails = validation.error.details.map((detail) => detail.message);
+      if (validation.error) {
+        const errorDetails = validation.error.details.map(
+          (detail) => detail.message
+        );
 
-                return {
-                    status: false,
-                    code: 422,
-                    error: errorDetails.join(", "),
-                };
-            }
+        return {
+          status: false,
+          code: 422,
+          error: errorDetails.join(", "),
+        };
+      }
 
-            const list = await prisma.nilai.findMany({
-                where: {
-                    mahasiswa: {
-                        mahasiswa_kecamatan_active: {
-                            id_kecamatan,
-                        },
+      const list = await prisma.nilai.findMany({
+        where: {
+          mahasiswa: {
+            mahasiswa_kecamatan_active: {
+              id_kecamatan,
+            },
+          },
+        },
+        include: {
+          mahasiswa: {
+            include: {
+              prodi: {
+                select: {
+                  nama: true,
+                  fakultas: {
+                    select: {
+                      nama: true,
                     },
+                  },
                 },
-                include: {
-                    mahasiswa: {
-                        include: {
-                            prodi: {
-                                select: {
-                                    nama: true,
-                                    fakultas: {
-                                        select: {
-                                            nama: true,
-                                        },
-                                    },
-                                },
-                            },
-                        },
+              },
+            },
+          },
+        },
+      });
+
+      return {
+        status: true,
+        data: list,
+      };
+    } catch (error) {
+      console.error("listNilaiKecamatan module error ", error);
+
+      return {
+        status: false,
+        error,
+      };
+    }
+  };
+
+  getNilai = async (id_nilai) => {
+    try {
+      const schema = Joi.number().required();
+
+      const validation = schema.validate(id_nilai);
+
+      if (validation.error) {
+        const errorDetails = validation.error.details.map(
+          (detail) => detail.message
+        );
+
+        return {
+          status: false,
+          code: 422,
+          error: errorDetails.join(", "),
+        };
+      }
+
+      const nilai = await prisma.nilai.findUnique({
+        where: {
+          id_nilai,
+        },
+        include: {
+          mahasiswa: {
+            include: {
+              prodi: {
+                select: {
+                  nama: true,
+                  fakultas: {
+                    select: {
+                      nama: true,
                     },
+                  },
                 },
-            });
+              },
+            },
+          },
+        },
+      });
 
-            return {
-                status: true,
-                data: list,
-            };
-        } catch (error) {
-            console.error("listNilaiKecamatan module error ", error);
+      return {
+        status: true,
+        data: nilai,
+      };
+    } catch (error) {
+      console.error("getNilai module error ", error);
 
-            return {
-                status: false,
-                error,
-            };
-        }
-    };
+      return {
+        status: false,
+        error,
+      };
+    }
+  };
 }
 
 module.exports = new _nilai();
