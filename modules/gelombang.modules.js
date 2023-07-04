8da63969c1d3bd15cd63b0fdbe3e935a4836cb67
oozaw <79121990+oozaw@users.noreply.github.com>
2023-07-04 13:38:03 +0700

    (^) list gelombang mahasiswa method

--- modules/gelombang.modules.js
+++ modules/gelombang.modules.js
@@ -2,196 +2,206 @@ const { prisma } = require("../helpers/database");
 const Joi = require("joi");
 
 class _gelombang {
-    listGelombang = async (id_tema, id_halaman) => {
-        try {
-            const body = {
-                id_tema,
-                id_halaman,
-            };
+  listGelombang = async (id_tema, id_halaman) => {
+    try {
+      const body = {
+        id_tema,
+        id_halaman,
+      };
 
-            const schema = Joi.object({
-                id_tema: Joi.number().required(),
-                id_halaman: Joi.number().required(),
-            });
+      const schema = Joi.object({
+        id_tema: Joi.number().required(),
+        id_halaman: Joi.number().required(),
+      });
 
-            const validation = schema.validate(body);
+      const validation = schema.validate(body);
 
-            if (validation.error) {
-                const errorDetails = validation.error.details.map((detail) => detail.message);
+      if (validation.error) {
+        const errorDetails = validation.error.details.map(
+          (detail) => detail.message
+        );
 
-                return {
-                    status: false,
-                    code: 422,
-                    error: errorDetails.join(", "),
-                };
-            }
+        return {
+          status: false,
+          code: 422,
+          error: errorDetails.join(", "),
+        };
+      }
 
-            const list = await prisma.gelombang.findMany({
-                where: {
-                    tema_halaman: {
-                        id_tema: body.id_tema,
-                        id_halaman: body.id_halaman,
-                    },
-                },
-            });
+      const list = await prisma.gelombang.findMany({
+        where: {
+          tema_halaman: {
+            id_tema: body.id_tema,
+            id_halaman: body.id_halaman,
+          },
+        },
+      });
 
-            return {
-                status: true,
-                data: list,
-            };
-        } catch (error) {
-            console.error("listGelombang module error ", error);
+      return {
+        status: true,
+        data: list,
+      };
+    } catch (error) {
+      console.error("listGelombang module error ", error);
 
-            return {
-                status: false,
-                error,
-            };
-        }
-    };
+      return {
+        status: false,
+        error,
+      };
+    }
+  };
 
-    listGelombangDosen = async (id_tema, id_halaman, id_dosen) => {
-        try {
-            const body = {
-                id_tema,
-                id_halaman,
-                id_dosen,
-            };
+  listGelombangDosen = async (id_tema, id_halaman, id_dosen) => {
+    try {
+      const body = {
+        id_tema,
+        id_halaman,
+        id_dosen,
+      };
 
-            const schema = Joi.object({
-                id_tema: Joi.number().required(),
-                id_halaman: Joi.number().required(),
-                id_dosen: Joi.number().required(),
-            });
+      const schema = Joi.object({
+        id_tema: Joi.number().required(),
+        id_halaman: Joi.number().required(),
+        id_dosen: Joi.number().required(),
+      });
 
-            const validation = schema.validate(body);
+      const validation = schema.validate(body);
 
-            if (validation.error) {
-                const errorDetails = validation.error.details.map((detail) => detail.message);
+      if (validation.error) {
+        const errorDetails = validation.error.details.map(
+          (detail) => detail.message
+        );
 
-                return {
-                    status: false,
-                    code: 422,
-                    error: errorDetails.join(", "),
-                };
-            }
+        return {
+          status: false,
+          code: 422,
+          error: errorDetails.join(", "),
+        };
+      }
 
-            const list = await prisma.gelombang.findMany({
-                where: {
-                    tema_halaman: {
-                        id_tema: body.id_tema,
-                        id_halaman: body.id_halaman,
-                    },
-                },
-                include: {
-                    proposal: {
-                        where: {
-                            id_dosen: body.id_dosen,
+      const list = await prisma.gelombang.findMany({
+        where: {
+          tema_halaman: {
+            id_tema: body.id_tema,
+            id_halaman: body.id_halaman,
+          },
+        },
+        include: {
+          proposal: {
+            where: {
+              id_dosen: body.id_dosen,
+            },
+            include: {
+              kecamatan: {
+                select: {
+                  nama: true,
+                  kabupaten: {
+                    select: {
+                      bappeda: {
+                        select: {
+                          nama_kabupaten: true,
                         },
-                        include: {
-                            kecamatan: {
-                                select: {
-                                    nama: true,
-                                    kabupaten: {
-                                        select: {
-                                            bappeda: {
-                                                select: {
-                                                    nama_kabupaten: true,
-                                                },
-                                            },
-                                        },
-                                    },
-                                },
-                            },
-                        },
+                      },
                     },
+                  },
                 },
-            });
+              },
+            },
+          },
+        },
+      });
 
-            return {
-                status: true,
-                data: list,
-            };
-        } catch (error) {
-            console.error("listGelombangDosen module error ", error);
+      return {
+        status: true,
+        data: list,
+      };
+    } catch (error) {
+      console.error("listGelombangDosen module error ", error);
 
-            return {
-                status: false,
-                error,
-            };
-        }
-    };
+      return {
+        status: false,
+        error,
+      };
+    }
+  };
 
-    listGelombangMahasiswa = async (id_tema, id_halaman, id_mahasiswa) => {
-        try {
-            const body = {
-                id_tema,
-                id_halaman,
-                id_mahasiswa,
-            };
+  listGelombangMahasiswa = async (id_tema, id_halaman, id_mahasiswa) => {
+    try {
+      const body = {
+        id_tema,
+        id_halaman,
+        id_mahasiswa,
+      };
 
-            const schema = Joi.object({
-                id_tema: Joi.number().required(),
-                id_halaman: Joi.number().required(),
-                id_mahasiswa: Joi.number().required(),
-            });
+      const schema = Joi.object({
+        id_tema: Joi.number().required(),
+        id_halaman: Joi.number().required(),
+        id_mahasiswa: Joi.number().required(),
+      });
 
-            const validation = schema.validate(body);
+      const validation = schema.validate(body);
 
-            if (validation.error) {
-                const errorDetails = validation.error.details.map((detail) => detail.message);
+      if (validation.error) {
+        const errorDetails = validation.error.details.map(
+          (detail) => detail.message
+        );
 
-                return {
-                    status: false,
-                    code: 422,
-                    error: errorDetails.join(", "),
-                };
-            }
+        return {
+          status: false,
+          code: 422,
+          error: errorDetails.join(", "),
+        };
+      }
 
-            const list = await prisma.gelombang.findMany({
-                where: {
-                    tema_halaman: {
-                        id_tema: body.id_tema,
-                        id_halaman: body.id_halaman,
-                    },
-                },
-                include: {
-                    mahasiswa_kecamatan: {
-                        where: {
-                            id_mahasiswa: body.id_mahasiswa,
+      const list = await prisma.gelombang.findMany({
+        where: {
+          tema_halaman: {
+            id_tema: body.id_tema,
+            id_halaman: body.id_halaman,
+          },
+        },
+        include: {
+          mahasiswa_kecamatan: {
+            where: {
+              id_mahasiswa: body.id_mahasiswa,
+            },
+            include: {
+              kecamatan: {
+                select: {
+                  nama: true,
+                  kabupaten: {
+                    select: {
+                      bappeda: {
+                        select: {
+                          nama_kabupaten: true,
                         },
-                        include: {
-                            kecamatan: {
-                                select: {
-                                    nama: true,
-                                    kabupaten: {
-                                        select: {
-                                            bappeda: {
-                                                select: {
-                                                    nama_kabupaten: true,
-                                                },
-                                            },
-                                        },
-                                    },
-                                },
-                            },
-                        },
+                      },
                     },
+                  },
                 },
-            });
+              },
+            },
+          },
+        },
+      });
 
-            return {
-                status: true,
-                data: list,
-            };
-        } catch (error) {
-            console.error("listGelombangMahasiswa module error ", error);
+      list.forEach((item) => {
+        item.jumlah_pendaftaran = item.mahasiswa_kecamatan.length;
+      });
 
-            return {
-                status: false,
-                error,
-            };
-        }
-    };
+      return {
+        status: true,
+        data: list,
+      };
+    } catch (error) {
+      console.error("listGelombangMahasiswa module error ", error);
+
+      return {
+        status: false,
+        error,
+      };
+    }
+  };
 }
 
 module.exports = new _gelombang();
