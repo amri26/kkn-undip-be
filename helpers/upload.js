4cd5c8b906b1b17788007280aadd9aeb7dde666c
oozaw <79121990+oozaw@users.noreply.github.com>
2023-07-06 11:30:38 +0700

    (^) upload drive method

--- helpers/upload.js
+++ helpers/upload.js
@@ -5,77 +5,77 @@ const stream = require("stream");
 const SCOPES = ["https://www.googleapis.com/auth/drive"];
 
 const auth = new google.auth.GoogleAuth({
-    keyFile: process.env.KEYFILEPATH,
-    scopes: SCOPES,
+  keyFile: process.env.KEYFILEPATH,
+  scopes: SCOPES,
 });
 
 const uploadDrive = async (fileObject, fileType, parentsFolder) => {
-    try {
-        const mime = fileObject.mimetype.split("/");
-        const extension = mime[mime.length - 1];
-        const filename = `${fileType}_${Date.now()}.${extension}`;
+  try {
+    const mime = fileObject.mimetype.split("/");
+    const extension = mime[mime.length - 1];
+    const filename = `${fileType}_${Date.now()}.${extension}`;
 
-        const bufferStream = new stream.PassThrough();
-        bufferStream.end(fileObject.buffer);
+    const bufferStream = new stream.PassThrough();
+    bufferStream.end(fileObject.buffer);
 
-        if (fileObject.mimeType === application / pdf) {
-            const file = await google.drive({ version: "v3", auth }).files.create({
-                media: {
-                    mimeType: fileObject.mimeType,
-                    body: bufferStream,
-                },
-                requestBody: {
-                    name: filename,
-                    parents: [parentsFolder],
-                },
-                fields: "id",
-            });
+    const file = await google.drive({ version: "v3", auth }).files.create({
+      media: {
+        mimeType: fileObject.mimeType,
+        body: bufferStream,
+      },
+      requestBody: {
+        name: filename,
+        parents: [parentsFolder],
+      },
+      fields: "id",
+    });
 
-            if (file.status !== 200) {
-                throw error;
-            }
-        } else {
-            throw error;
-        }
-
-        return file;
-    } catch (error) {
-        throw error;
+    if (file.status !== 200) {
+      throw error;
     }
+    // if (fileObject.mimeType === "application/pdf") {
+    // } else {
+    //   throw error;
+    // }
+
+    return file;
+  } catch (error) {
+    throw error;
+  }
 };
 
 const downloadDrive = async (fileId) => {
-    try {
-        const file = await google.drive({ version: "v3", auth }).files.get({
-            fileId,
-            fields: "webContentLink", //"webContentLink,name,id"
-        });
+  try {
+    const file = await google.drive({ version: "v3", auth }).files.get({
+      fileId,
+      fields: "webContentLink", //"webContentLink,name,id"
+    });
 
-        if (file.status !== 200) {
-            throw error;
-        }
-
-        return file;
-    } catch (error) {
-        throw error;
+    if (file.status !== 200) {
+      throw error;
     }
+
+    return file;
+  } catch (error) {
+    throw error;
+  }
 };
 
 const embedLinkDrive = async (fileId) => {
-    try {
-        const file = await google.drive({ version: "v2", auth }).files.get({
-            fileId,
-            fields: "embedLink",
-        });
+  try {
+    const file = await google.drive({ version: "v2", auth }).files.get({
+      fileId,
+      fields: "embedLink",
+    });
 
-        if (file.status !== 200) {
-            throw error;
-        }
-
-        return file;
-    } catch (error) {
-        throw error;
+    if (file.status !== 200) {
+      throw error;
     }
+
+    return file;
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = { uploadDrive, downloadDrive, embedLinkDrive };
