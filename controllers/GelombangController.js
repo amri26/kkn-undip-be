6ac1c009c17c5a02049bb01919f4c7f6d3540843
oozaw <79121990+oozaw@users.noreply.github.com>
2023-07-26 10:23:21 +0700

    fix gelombang

--- controllers/GelombangController.js
+++ controllers/GelombangController.js
@@ -1,20 +1,106 @@
 const { Router } = require("express");
 const modules = require("../modules/gelombang.modules");
 const response = require("../helpers/response");
-const { userSession } = require("../helpers/middleware");
+const { userSession, verifyAdmin } = require("../helpers/middleware");
 
 const app = Router();
 
-app.get("/:id_tema/:id_halaman", userSession, async (req, res, next) => {
-    response.sendResponse(res, await modules.listGelombang(Number(req.params.id_tema), Number(req.params.id_halaman)));
+app.get("/:id_tema", userSession, verifyAdmin, async (req, res, next) => {
+  response.sendResponse(
+    res,
+    await modules.listGelombang(Number(req.params.id_tema))
+  );
 });
 
-app.get("/dosen/:id_tema/:id_halaman/:id_dosen", userSession, async (req, res, next) => {
-    response.sendResponse(res, await modules.listGelombangDosen(Number(req.params.id_tema), Number(req.params.id_halaman), Number(req.params.id_dosen)));
+app.get(
+  "/halaman/:id_tema/:id_halaman",
+  userSession,
+  async (req, res, next) => {
+    response.sendResponse(
+      res,
+      await modules.listGelombangHalaman(
+        Number(req.params.id_tema),
+        Number(req.params.id_halaman)
+      )
+    );
+  }
+);
+
+app.get(
+  "/dosen/:id_tema/:id_halaman/:id_dosen",
+  userSession,
+  async (req, res, next) => {
+    response.sendResponse(
+      res,
+      await modules.listGelombangDosen(
+        Number(req.params.id_tema),
+        Number(req.params.id_halaman),
+        Number(req.params.id_dosen)
+      )
+    );
+  }
+);
+
+app.get(
+  "/mahasiswa/:id_tema/:id_halaman/:id_mahasiswa",
+  userSession,
+  async (req, res, next) => {
+    response.sendResponse(
+      res,
+      await modules.listGelombangMahasiswa(
+        Number(req.params.id_tema),
+        Number(req.params.id_halaman),
+        Number(req.params.id_mahasiswa)
+      )
+    );
+  }
+);
+
+app.get(
+  "/detail/:id_gelombang",
+  userSession,
+  verifyAdmin,
+  async (req, res, next) => {
+    response.sendResponse(
+      res,
+      await modules.getGelombang(Number(req.params.id_gelombang))
+    );
+  }
+);
+
+app.post("/", userSession, verifyAdmin, async (req, res, next) => {
+  response.sendResponse(res, await modules.addGelombang(req.body));
 });
 
-app.get("/mahasiswa/:id_tema/:id_halaman/:id_mahasiswa", userSession, async (req, res, next) => {
-    response.sendResponse(res, await modules.listGelombangMahasiswa(Number(req.params.id_tema), Number(req.params.id_halaman), Number(req.params.id_mahasiswa)));
+app.put("/:id_gelombang", userSession, verifyAdmin, async (req, res, next) => {
+  response.sendResponse(
+    res,
+    await modules.editGelombang(Number(req.params.id_gelombang), req.body)
+  );
 });
+
+app.patch(
+  "/:id_gelombang",
+  userSession,
+  verifyAdmin,
+  async (req, res, next) => {
+    response.sendResponse(
+      res,
+      await modules.switchGelombang(Number(req.params.id_gelombang))
+    );
+  }
+);
+
+app.delete(
+  "/:id_gelombang",
+  userSession,
+  verifyAdmin,
+  async (req, res, next) => {
+    response.sendResponse(
+      res,
+      await modules.deleteGelombang(Number(req.params.id_gelombang))
+    );
+  }
+);
 
 module.exports = app;
